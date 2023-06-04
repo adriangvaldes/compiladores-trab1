4187eb170e58a32429af7728b4b92b8153f8ed0b
adriangvaldes <adriangvaldes@gmail.com>
2023-06-04 17:00:12 -0300

    finish trab2

--- src/trab2.js
+++ src/trab2.js
@@ -27,10 +27,10 @@ function extractVariable(pilha, type, line) {
   }
 }
 
-function printVariable(pilha, line) { // Funçao para processar os PRINT's no código
+function printVariable(pilha, line, numeroLinha) { // Funçao para processar os PRINT's no código
   const [print, variable] = line.split(' ');
   const escopo = pilha.find(escopo => escopo.variaveis[variable])
-  if (!escopo) console.log('\x1b[31m%s\x1b[0m', `Variável ${variable} não declarada`) //Se a variavel encontrada n for achado no escopo, ela é assumida como não declarada
+  if (!escopo) console.log('\x1b[31m%s\x1b[0m', `[Linha: ${numeroLinha}] - Variável ${variable} não declarada`) //Se a variavel encontrada n for achado no escopo, ela é assumida como não declarada
   else console.log(`Print ${variable}: `, `${escopo.variaveis[variable].value}`)
 }
 
@@ -38,18 +38,18 @@ function isNumber(variable) {
   return !isNaN(parseInt(variable)); // Utiliza isNaN que basicamente é um metodo que vai retornar true ou false se o elemento verificado não é um numero
 }
 
-function changeVariableValueOnAtribution(pilha, line) { //Função para tratar atribuições após as declarações
+function changeVariableValueOnAtribution(pilha, line, numeroLinha) { //Função para tratar atribuições após as declarações
   const regex = /(\w+)\s*=\s*([^,]+)/g; // Expressão regular para identificar cadeias de caracteres do tipo 'x = v' ou 'x' onde v pode ser tanto uma string qnt um número
   let match;
   while ((match = regex.exec(line)) !== null) {
     const variable = match[1];
     const value = match[2] ?? null;
     const escopo = pilha.find(escopo => escopo.variaveis[variable])
-    if (!escopo) console.log('\x1b[31m%s\x1b[0m', `Variável ${variable} não declarada`)
+    if (!escopo) console.log('\x1b[31m%s\x1b[0m', `[Linha: ${numeroLinha}] - Variável ${variable} não declarada`)
     else {
       const variableStorageType = pilha[0].variaveis[variable].type
-      if (isNumber(value) && variableStorageType === 'string' || !isNumber(value) && variableStorageType === 'number') {
-        console.log('\x1b[31m%s\x1b[0m', `Variável "${variable}" declarada anteriormente como "${variableStorageType}"`)
+      if (isNumber(value) && variableStorageType === 'string' || !isNumber(value) && variableStorageType === 'number') { // Verificação se atribuiçao corresponde ao tipo da variavel a ser mudada
+        console.log('\x1b[31m%s\x1b[0m', `[Linha: ${numeroLinha}] - Variável "${variable}" declarada anteriormente como "${variableStorageType}"`)
       } else {
         pilha[0].variaveis[variable] = {
           ...pilha[0].variaveis[variable],
@@ -63,18 +63,22 @@ function changeVariableValueOnAtribution(pilha, line) { //Função para tratar a
 function analisadorSemantico(codigo) {
   const pilhaEscopo = [];
   const linhaPorLinha = codigo.split('\n').map(linha => linha.trim()).filter(line => line !== '');
+  console.log('+-----------------------------------------------------------------------------------------+');
+  console.log('|                           ANALISADOR SEMANTICO - ADRIAN VALDES                          |');
+  console.log('+-----------------------------------------------------------------------------------------+');
+  console.log('\n Prints do programa lido:');
 
   linhaPorLinha.forEach((line, index) => {
-    const linhaCount = index + 1;
+    const linhaCount = index + 2;
 
-    if (line.includes('BLOCO')) {
+    if (line.includes('BLOCO')) { //Caso identifique a palavra BLOCO o programa vai assumir q essa parte do código ja é um novo escopo e adicionar à pilha de escopos
       pilhaEscopo.unshift({
         escopo: pilhaEscopo.length + 1,
         variaveis: null
       })
     }
-    if (line.includes('FIM')) {
-      const element = pilhaEscopo.shift();
+    if (line.includes('FIM')) { // Ao chegar em um final bloco aquele escopo referente (o ultimo adicionado) a esse fim, é tirado da pilha
+      pilhaEscopo.shift();
     }
 
 
@@ -83,13 +87,12 @@ function analisadorSemantico(codigo) {
     } else if (line.includes('CADEIA')) {
       extractVariable(pilhaEscopo, 'string', line);
     } else {
-      changeVariableValueOnAtribution(pilhaEscopo, line)
+      changeVariableValueOnAtribution(pilhaEscopo, line, linhaCount)
     }
     if (line.includes('PRINT')) {
-      printVariable(pilhaEscopo, line)
+      printVariable(pilhaEscopo, line, linhaCount)
     }
   })
-  // console.log(JSON.stringify(pilhaEscopo, null, 2))
 }
 
 
